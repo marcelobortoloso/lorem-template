d684ba948a245178a7a5ad19814ce4edf73f85d2
Marcelo Bortoloso <marcelobortoloso@outlook.com>
2023-01-19 14:08:53 -0300

    Primeiro commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(".brands").slick({
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  dots: false,
+  arrows: true,
+  infinite: true,
+});
+
+$(".blog").slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  dots: false,
+  arrows: true,
+  infinite: true,
+});
